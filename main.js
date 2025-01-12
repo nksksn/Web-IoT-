b5a6587cccd755761ccb19df88e4f2012bd81a0d
Ariq Prabaswara <128021114+ariq-prabaswara@users.noreply.github.com>
2025-01-12 15:14:50 +0900

    ジャイロセンサープログラムを追

--- main.js
+++ main.js
@@ -25,6 +25,12 @@ async function main() {
   var port2 = i2CAccess.ports.get(1);
   var mpu6050 = new MPU6050(port2, 0x68);
   await mpu6050.init();
+
+  var i = 0; 
+  var count = 0; // 術発動用の時間カウンター
+  var temp_rx = 0;
+  var temp_ry = 0;
+  var temp_rz = 0;
   
   //GPSの初期設定
   const port = new SerialPort({ path: '/dev/ttyS0', baudRate: 9600 })
@@ -34,26 +40,57 @@ async function main() {
     gps.update(txtData);
   });
 
+  // webSocketリレーの初期化
   var relay = RelayServer("achex", "chirimenSocket",nodeWebSocketLib,"https://chirimen.org");
   channel = await relay.subscribe("chirimenMbitSensors");
+  console.log("web socketリレーサービスに接続しました");
 
-  while (true) {
-      const data = await mpu6050.readAll();
-      const temperature = data.temperature.toFixed(2);
-      const g = [data.gx, data.gy, data.gz];
+  // センサーの初期化
+  while (i < 100){
+    const data_mpu = await mpu6050.readAll();
+    temp_rx = temp_rx + data_mpu.rx;
+    temp_ry = temp_ry + data_mpu.ry;
+    temp_rz = temp_rz + data_mpu.rz;
+    i = i + 1;
+  }
+  const err_rx = temp_rx/i; //3.783; // Error Rx
+  const err_ry = temp_ry/i; //0.487; // Error Ry
+  const err_rz = temp_rz/i; //-0.01; // Error Rz
+  
+  // ディバッグ用：
+  console.log(
+  [
+  `/////////////////// 初期化TEST //////////////////////`,	
+  `temp_rx: ${temp_rx}, temp_ry: ${temp_ry}, temp_rz: ${temp_rz}`,
+  `err_rx: ${err_rx}, err_ry: ${err_ry}, err_rz: ${err_rz}`,
+  `////////////////////// TEST ////////////////////////`
+  ].join("\n")
+  );
       
-      console.log(
-        [
-          `Temperature: ${temperature} degree`,
-          `Gx: ${g[0]}, Gy: ${g[1]}, Gz: ${g[2]}`,
-        ].join("\n")
-      );
+  while (true) {
+      const data_mpu = await mpu6050.readAll();
+      const temperature = data_mpu.temperature.toFixed(2);
+      const g = [data_mpu.gx, data_mpu.gy, data_mpu.gz];
+      const r = [data_mpu.rx - err_rx, data_mpu.ry - err_ry, data_mpu.rz - err_rz];
      
-     const acc = Math.sqrt(g[0]**2+g[1]**2+g[2]**2).toFixed(1);
-     console.log(`Accelation:${acc}`)//コンソール用
-     
-     gps.on('data', function (data) {
-        if (data.type == "RMC") { // "RMC"タイプデータを読むと速度(ノット)が得られる
+     //　加速度を計算
+	  const acc = Math.sqrt(r[0]**2+r[1]**2+r[2]**2).toFixed(1);
+    //console.log(`Accelation:${acc}`)//コンソール用
+    
+    // ディバッグ用：
+    console.log(
+      [
+      //`Temperature: ${temperature} degree`,
+      //`Gx: ${g[0].toFixed(2)}, Gy: ${g[1].toFixed(2)}, Gz: ${g[2].toFixed(2)}`,
+      `Gx: ${g[0].toFixed(2)}`,
+      `count: ${count}`,
+      //`Rx: ${r[0].toFixed(1)}, Ry: ${r[1].toFixed(1)}, Rz: ${r[2].toFixed(1)}`,
+      `Acceleration: ${acc}`
+      ].join("\n")
+    );
+
+    gps.on('data', function (data) {
+      if (data.type == "RMC") { // "RMC"タイプデータを読むと速度(ノット)が得られる
         const gpsData = {
           latitude: gps.state.lat,
           longitude: gps.state.lon,
@@ -62,14 +99,34 @@ async function main() {
         };
       channel.send(JSON.stringify(gpsData)); // Send
       console.log(JSON.stringify(gpsData))
-    }
-  });
-     
-     if(acc > 40){
-         port1.write(1)//ブザーが鳴る
-     }
+      }
+    });
+  
+    // サーバにメッセージを送信する
+    if (g[0] > 0.8 && acc < 100) {
+      channel.send(`SAFE`);
+      console.log(`SAFE`);
+    } else if (g[0] > -0.20 && g[0] < 0.20 && acc < 100) {
+      count = count + 1;
+      
+      // 保持時間＝count*loop await sleep 時間
+      if (count > 6){
+        channel.send(`WAZA`);
+        console.log(`WAZA`);
+        count = 0;
+      }
+    } else {
+      channel.send(`DANGER`);
+      console.log(`DANGER`);
+      count = 0;
+    } 
 
-     await sleep(500);
+    //ブザー
+    if(acc > 100){
+         port1.write(1)//ブザーが鳴る
+    }
+
+    await sleep(500);
   }
 
 }
